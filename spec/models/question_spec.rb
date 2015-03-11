641e69b91fd50c94594164e550789deb6295814e
Alex Ivanov <merch2@yandex.ru>
2015-03-11 16:12:47 +0600

    lesson 3.4

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,14 +4,6 @@ RSpec.describe Question, type: :model do
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  it { should have_many :answers }
-
-  it 'should delete dependent answers' do
-      q  = FactoryGirl.create(:question)
-      a1 = FactoryGirl.create(:answer, question_id: q.id)
-      a2 = FactoryGirl.create(:answer, question_id: q.id)
-      a3 = FactoryGirl.create(:answer, question_id: q.id)
-      expect { q.destroy }.to change { Answer.count }.by(-3)
-  end
+  it { should have_many(:answers).dependent(:destroy) }
 
 end
