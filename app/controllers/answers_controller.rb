964444bc7903714fd7aa4ee604f0a3b310be2b35
Alex Ivanov <merch2@yandex.ru>
2015-03-14 17:26:16 +0600

    lesson 4.3

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,14 @@
 class AnswersController < ApplicationController
+
   def new
     @answer = Answer.new
   end
 
   def create
-    @answer = Answer.new(answers_params)
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answers_params)
     if @answer.save
-       @answer.question_id = params[:question_id]
-       @answer.save
-      redirect_to root_path
+      redirect_to @question
     else
       render :new
     end
@@ -19,4 +19,5 @@ class AnswersController < ApplicationController
   def answers_params
     params.require(:answer).permit(:body)
   end
+
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -7,30 +7,32 @@ RSpec.describe AnswersController, type: :controller do
     it 'new Answer eq @answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
     end
+
     it 'render new page' do
       expect(response).to render_template :new
     end
   end
 
   describe 'POST #create' do
+
     context 'valid attributes' do
       it 'save answer in db' do
-        expect { post :create, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+        expect { post :create, question_id: question, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
       end
 
       it 'redirect to show page' do
-        post :create, answer: attributes_for(:answer)
-        expect(response).to redirect_to root_path
+        post :create, question_id: question, answer: attributes_for(:answer)
+        expect(response).to redirect_to question_path(question)
       end
     end
 
     context 'invalid attributes' do
       it 'doe not save answer in db' do
-        expect { post :create, answer: attributes_for(:invalid_answer) }.to_not change(Answer, :count)
+        expect { post :create, question_id: question, answer: attributes_for(:invalid_answer) }.to_not change(Answer, :count)
       end
 
       it 'render to show page' do
-        post :create, answer: attributes_for(:invalid_answer)
+        post :create, question_id: question, answer: attributes_for(:invalid_answer)
         expect(response).to render_template :new
       end
     end
