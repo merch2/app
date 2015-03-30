526cfd6c1219dda9ae44d5b5c20c09a9420555c3
Alex Ivanov <merch2@yandex.ru>
2015-03-30 16:58:32 +0600

    lesson 8.4

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -4,6 +4,5 @@ class AttachmentsController < ApplicationController
     @question = Question.find(params[:question_id])
     @attachment = Attachment.find(params[:id])
     @attachment.destroy!
-    redirect_to question_path(@question)
   end
 end
