117fe50837fb155bea87a4aa6c7af02e77c4334c
Alex Ivanov <merch2@yandex.ru>
2015-05-04 21:41:23 +0600

    lesson 12.8

--- spec/acceptance/omniauth_spec.rb
+++ spec/acceptance/omniauth_spec.rb
@@ -6,8 +6,9 @@ feature 'Omniauth' do
     OmniAuth.config.test_mode = true
     visit root_path
     click_on 'log in'
-    click_on 'Sign in with Facebook'
     mock_auth_hash(:facebook, '123@123.com')
+    click_on 'Sign in with Facebook'
+
 
     expect(page).to have_content 'Successfully authenticated from Facebook account'
   end
@@ -16,8 +17,9 @@ feature 'Omniauth' do
     OmniAuth.config.test_mode = true
     visit root_path
     click_on 'log in'
-    click_on 'Sign in with Twitter'
     mock_auth_hash(:twitter)
+    click_on 'Sign in with Twitter'
+
     expect(page).to have_content 'Please fill form'
     fill_in 'auth_info_email', with: 'test@email.com'
     click_on 'Save'
